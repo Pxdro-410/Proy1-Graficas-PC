5be7a463d850e3620a4d979c20cf78f00c2ae23d
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-08-07 01:20:14 -0600

    feat: modificacion en el caster para permitir el renderizar las pruebas en simulacion de 3D

--- src/caster.rs
+++ src/caster.rs
@@ -1,3 +1,4 @@
+use crate::cell_color;
 use crate::framebuffer::Framebuffer;
 use crate::maze::Maze;
 use crate::player::Player;
@@ -8,28 +9,47 @@ pub fn cast_ray(
     player: &Player,
     a: f32,
     block_size: usize,
+    i: usize,
 ) {
     let mut d = 0.0;
-
-    framebuffer.set_current_color(0xFFDDDD);
 
     loop {
         let x = (player.pos.x + d * a.cos()) as usize;
         let y = (player.pos.y + d * a.sin()) as usize;
 
-        let i = x / block_size;
-        let j = y / block_size;
+        let col = x / block_size;
+        let row = y / block_size;
 
-        if j >= maze.len() || i >= maze[j].len() {
+        if row >= maze.len() || col >= maze[row].len() {
             return;
         }
 
-        if maze[j][i] != ' ' {
+        if maze[row][col] != ' ' {
+            let cell = maze[row][col];
+
+            // Color obtenido desde la función cell_color en main.rs
+            let color = cell_color(cell);
+
+            // Corrección de fish-eye
+            let distance = d * (a - player.a).cos();
+
+            // Altura de la pared en 3D
+            let d_plane = 500.0;
+            let wall_height = (block_size as f32 / distance) * d_plane;
+
+            let h = framebuffer.height as f32;
+            let draw_start = ((h / 2.0) - (wall_height / 2.0)).max(0.0) as usize;
+            let draw_end = ((h / 2.0) + (wall_height / 2.0)).min(h) as usize;
+
+            // Dibujar la tira vertical de la pared
+            framebuffer.set_current_color(color);
+            for y_pix in draw_start..draw_end {
+                framebuffer.point(i, y_pix);
+            }
             return;
         }
-
-        framebuffer.point(x, y);
 
         d += 1.0;
     }
 }
+

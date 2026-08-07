acd33af177b51b72d3a95745bd6434c4bfc0ae7b
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-08-07 01:50:18 -0600

    feat: agregar logica para renderizar las celdas en 2D y mostrar los 5 rayos al cambiar de 3D a 2D en el caster

--- src/caster.rs
+++ src/caster.rs
@@ -3,6 +3,36 @@ use crate::framebuffer::Framebuffer;
 use crate::maze::Maze;
 use crate::player::Player;
 
+pub fn cast_ray_2d(
+    framebuffer: &mut Framebuffer,
+    maze: &Maze,
+    player: &Player,
+    a: f32,
+    block_size: usize,
+) {
+    let mut d = 0.0;
+    framebuffer.set_current_color(0xFFDDDD);
+
+    loop {
+        let x = (player.pos.x + d * a.cos()) as usize;
+        let y = (player.pos.y + d * a.sin()) as usize;
+
+        let col = x / block_size;
+        let row = y / block_size;
+
+        if row >= maze.len() || col >= maze[row].len() {
+            return;
+        }
+
+        if maze[row][col] != ' ' {
+            return;
+        }
+
+        framebuffer.point(x, y);
+        d += 1.0;
+    }
+}
+
 pub fn cast_ray(
     framebuffer: &mut Framebuffer,
     maze: &Maze,
